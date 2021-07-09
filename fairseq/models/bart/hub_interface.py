e2eb21c71a63b733cbe01128f58e63af62d442e1
Nishant Yadav <nishanya@amazon.com>
2021-07-09 11:02:38 -0400

    Small bug fix in inference, copying translation support code for multilabel models. Will update this next

--- fairseq/models/bart/hub_interface.py
+++ fairseq/models/bart/hub_interface.py
@@ -55,11 +55,13 @@ class BARTHubInterface(GeneratorHubInterface):
         tokens = self.bpe.encode(sentence)
         if len(tokens.split(" ")) > min(self.max_positions) - 2:
             tokens = " ".join(tokens.split(" ")[: min(self.max_positions) - 2])
-        bpe_sentence = "<s> " + tokens + " </s>"
-        for s in addl_sentences:
-            bpe_sentence += " </s>" if not no_separator else ""
-            bpe_sentence += " " + self.bpe.encode(s) + " </s>"
-        tokens = self.task.source_dictionary.encode_line(bpe_sentence, append_eos=False)
+        # bpe_sentence = "<s> " + tokens + " </s>"
+        # for s in addl_sentences:
+        #     bpe_sentence += " </s>" if not no_separator else ""
+        #     bpe_sentence += " " + self.bpe.encode(s) + " </s>"
+
+        bpe_sentence = tokens + " </s>"  # Not including bos_token here as it was not included in training source data
+        tokens = self.task.source_dictionary.encode_line(bpe_sentence, add_if_not_exist=False, append_eos=False)
         return tokens.long()
 
     def decode(self, tokens: torch.LongTensor):
